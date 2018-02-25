951a966e399dcf1d8e696e6acbc86aaf2044d8cd
BoschetBasile <basile.boschet@ynov.com>
2018-02-25 23:40:15 +0100

    IonicProjetPizza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { PanierPage } from '../pages/panier/panier';
+import {PizzaAdminPage} from '../pages/pizza-admin/pizza-admin';
+import { PizzaServiceProvider } from '../providers/pizza-service/pizza-service';
+import { HttpClientModule } from '@angular/common/http';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    PizzaAdminPage,
+    PanierPage
+
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    PizzaAdminPage,
+
+
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PizzaServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {PizzaServiceProvider} from '../../providers/pizza-service/pizza-service';
+import {PizzaAdminPage} from '../pizza-admin/pizza-admin';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  pizzas: any;
 
-  constructor(public navCtrl: NavController) {
+  Admin: PizzaAdminPage;
 
+  constructor(public navCtrl: NavController, public pizzaServiceProvider : PizzaServiceProvider)
+  {
+    this.getPizza();
+  }
+
+  goToAdminPage() {
+  //push another page onto the history stack
+  //causing the nav controller to animate the new page in
+  this.navCtrl.push(PizzaAdminPage);
+}
+
+  getPizza() {
+    this.pizzaServiceProvider.getPizza()
+    .then(data => {
+      this.pizzas = data;
+      console.log(this.pizzas);
+    });
   }
 
 }
